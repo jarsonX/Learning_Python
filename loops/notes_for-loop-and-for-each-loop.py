cb963732b45052c72560187501617a7f54345756
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-05-25 08:56:26 +0200

    Create notes_for-loop-and-for-each-loop.py

---
+++ loops/notes_for-loop-and-for-each-loop.py
@@ -0,0 +1,38 @@
+#For loop--------------------------
+
+#for i in range(len(the_variable)):
+ #   the_value = the_variable[i]
+  #  …
+
+mystery_string = “Jarson”
+
+for x in range(len(mystery_string)):
+    print(x, mystery_string[x])
+
+# 0 J
+# 1 a
+# 2 r
+# 3 s
+# 4 o
+# 5 n
+    
+#For each loop---------------------
+
+#i = -1
+#for the_value in the_variable:
+ #   i += 1
+  #  …
+
+mystery_string = “Jarson”
+
+i = 0
+for x in mystery_string:
+    print(i, x)
+    i +=1
+
+# 0 J
+# 1 a
+# 2 r
+# 3 s
+# 4 o
+# 5 n
