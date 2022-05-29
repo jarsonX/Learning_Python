0b41d6ccd8872a41e5eef1790e22883cbddfb016
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-05-29 13:21:27 +0200

    Update notes_basics.py

--- data-structures/notes_basics.py
+++ data-structures/notes_basics.py
@@ -1,20 +1,20 @@
-FUNDAMENTALS
+#FUNDAMENTALS
+##########################################################################################
 
-  Mutability: Whether or not a variable can have its value changed after being declared. 
+#Mutability: Whether or not a variable can have its value changed after being declared. 
+#Mutable Variable: A variable whose value can change after it has been declared. 
+#Immutable Variable: A variable whose value cannot change after it has been declared.
+#Methods: Functions that are contained within data types.
 
-  Mutable Variable: A variable whose value can change after it has been declared. 
+#Adanced Data Structures:
 
-  Immutable Variable: A variable whose value cannot change after it has been declared.
-
-  Methods: Functions that are contained within data types.
-
-
-ADVANCED DATA STRUCTURES:
-
-  Multi-Dimensional Lists: A list-like structure where the items in a list are themselves lists, such that the practical effect is a multi-dimensional list.
-
-  Stack: A list-like structure that follows the “Last-In-First-Out” paradigm, where we can only access the most recently-added item on the list and can only access it by removing it from the list.
-
-  Queue: A list-like structure that follows the “First-In-First-Out” paradigm, where we can only access the least recently-added item on the list  and can only access it by removing it from the list.
-
-  Linked List: A list-like structure where the location of each item in the list is contained in the previous item in the list.
+#Multi-Dimensional Lists: A list-like structure where the items in a list are themselves lists, 
+  #such that the practical effect is a multi-dimensional list.
+#Stack: A list-like structure that follows the “Last-In-First-Out” paradigm, where we can only 
+  #access the most recently-added item on the list and can only access it by removing it from 
+  #the list.
+#Queue: A list-like structure that follows the “First-In-First-Out” paradigm, where we can only 
+  #access the least recently-added item on the list  and can only access it by removing it from 
+  #the list.
+#Linked List: A list-like structure where the location of each item in the list is contained in 
+  #the previous item in the list.
