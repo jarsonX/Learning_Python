311417bcc6356076f590ef9d6832cdd128a8e7b6
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-02 11:17:31 +0200

    Create notes_basics.py

---
+++ lib-pprint/notes_basics.py
@@ -0,0 +1,17 @@
+#Python comes with a built-in library called pprint, which stands for pretty-print. 
+#Using this library, we can print out more nicely formatted data structures, including 
+#dictionaries.
+
+#The pprint library allows us to specify a number of different parameters, including 
+#the following:
+
+    #indent: the number of spaces to indent each line, where the default value is 1
+    #width: the maximum caharacters that are allowed on a single line, where the default 
+      #is None (meaning, the maximum)
+    #depth: the number of levels to show while using nested data types, where the default 
+      #is None (meaning that all levels of depth are shown)
+    #stream: used to specify an output stream and can be used to save to a file
+    #compact: If set to True, the values will be printed on single lines, to a certain 
+      #width
+    #sort_dicts: if True, it prints the key-value pairs according to an alphabetical order 
+      #of the keys. Defaults to True
