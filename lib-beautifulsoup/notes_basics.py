4d6ea57ecb2e1ac5a8ce08eacc72f023d1f8b9e9
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-02 09:24:16 +0200

    Update notes_basics.py

--- lib-beautifulsoup/notes_basics.py
+++ lib-beautifulsoup/notes_basics.py
@@ -25,17 +25,17 @@ import requests  # this module helps us to download a web page
 %%html
 <!DOCTYPE html>
 <html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-<h3><b id='boldest'>Lebron James</b></h3>
-<p> Salary: $ 92,000,000 </p>
-<h3> Stephen Curry</h3>
-<p> Salary: $85,000, 000 </p>
-<h3> Kevin Durant </h3>
-<p> Salary: $73,200, 000</p>
-</body>
+  <head>
+    <title>Page Title</title>
+  </head>
+  <body>
+    <h3><b id='boldest'>Lebron James</b></h3>
+    <p> Salary: $ 92,000,000 </p>
+    <h3> Stephen Curry</h3>
+    <p> Salary: $85,000, 000 </p>
+    <h3> Kevin Durant </h3>
+    <p> Salary: $73,200, 000</p>
+  </body>
 </html>
 '''
 
