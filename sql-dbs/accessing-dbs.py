b2b60c6604b0f46ece2aa72aec356d1b9f64983d
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-11 12:55:26 +0200

    Update accessing-dbs.py

--- sql-dbs/accessing-dbs.py
+++ sql-dbs/accessing-dbs.py
@@ -1,1 +1,13 @@
+#DB-API CODE SCHEME
+####################################################################################################
 
+from dbmodule import connect
+
+connection = connect ('databasename', 'username', 'pswd')  #connection object
+cursor = connection.cursor()  #cursor object
+
+cursor.execute('select * from tablename')  #run queries
+results = cursor.fetchall()
+
+cursor.close()
+connection.close()  #free resources
