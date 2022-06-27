8cd5652eab66db74d76ed2dca68b80167815efc0
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-27 11:14:29 +0200

    Update >> notes_basics.py

--- objects/>> notes_basics.py
+++ objects/>> notes_basics.py
@@ -1,1 +1,15 @@
+----------------------------------------------------------------------------------------------THEORY
+____________________________________________________________________________________________________
 
+Object-oriented programming: a programming paradigm where programmers define custom data types that 
+have custom methods embedded within them.
+
+Object: An object is a custom data structure that organizes and encapsulates variables and methods 
+into a single data type. It is used near-interchangeably with “instance.”
+
+Class: A custom data type comprised of multiple variables and/or methods. Instances or objects are 
+created based on the template provided by the class.
+
+Instance: A single set of values of a particular class. Classes may be comprised of multiple 
+variables; an instance is a set of values for these variables. The term “instance” is often used
+interchangeably with the term “object”.
