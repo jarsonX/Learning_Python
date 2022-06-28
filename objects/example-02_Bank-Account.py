839af1d204b30be263ee0eb146b1e6cc7dd7dd45
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-28 13:32:32 +0200

    Update example-02_Bank-Account.py

--- objects/example-02_Bank-Account.py
+++ objects/example-02_Bank-Account.py
@@ -1,1 +1,29 @@
-
+class BankAccount:
+  
+  def __init__(self, name, balance = 0.0):
+    self.log("Account created")
+    self.name = name
+    self.balance = balance
+    
+  def getBalance(self):
+    self.log("Balance checked at " + str(self.balance))
+    return self.balance
+  
+  def deposit(self, amount):
+    self.balance += amount
+    self.log("+" + str(amount) + ": " + str(self.balance))
+    
+  def withdraw(self, amount):
+    self.balance -= amount
+    self.log("-" + str(amount) + ": " + str(self.balance))
+    
+  def log(self, message):
+    myLog = open("Log.txt", "a")
+    print(message, file = myLog)
+    myLog.close()
+    
+myBankAccount = BankAccount("Jarson")
+myBankAccount.deposit(20.0)
+print(myBankAccount.getBalance())
+myBankAccount.withdraw(10.0)
+print(myBankAccount.getBalance())
