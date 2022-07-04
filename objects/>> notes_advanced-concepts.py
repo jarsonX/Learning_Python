e851bfa0b5aec2a754bfd64cdc653374ea4cdef2
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-04 08:39:02 +0200

    Update and rename >> notes_advanced-concepts to >> notes_advanced-concepts.py

---
+++ objects/>> notes_advanced-concepts.py
@@ -0,0 +1,10 @@
+____________________________________________________________________________________________________
+Abstraction: #A principle of object-oriented programming that states that only essential information 
+#should be made visible to the outside program.
+
+Polymorphism: #The principle that a method call can behave differently depending on the arguments 
+#and object with which it is called.
+
+Inheritance: #A principle of object-oriented programming where classes can be created that are 
+#“subclasses” of other classes, inheriting all the variables and methods from the other class while 
+#supplying new variables, methods, or behaviors of these own.
