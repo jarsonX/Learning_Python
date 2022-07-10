df6e1b4d05d914bc7691532f8f5560df3f3b3052
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-10 11:25:55 +0200

    Create notes_pipelines.py

---
+++ ML-and-DS/notes_pipelines.py
@@ -0,0 +1,15 @@
+#--------------------------------------------------------------------Using-a-pipeline-in-scikit-learn
+#____________________________________________________________________________________________________
+
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+Input = [('scale', StandardScaler()), ('polynomial', PolynomialFeatures(degree=2, include_bias=False)) \
+         ('mode', LinearRegression())]  #'name of the estimator model', model constructor
+
+pipe = Pipeline(Input)
+pipe.fit(df[['var1', 'var2', 'var3']], y)  #train the pipeline
+
+yhat = pipe.predict(X[['var1', 'var2', 'var3']])  #produce a prediction
