8d67787acac06cbd981ace1ca48f1c9983c954be
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-12 15:06:08 +0200

    Update notes_search-algorithms.py

--- algorithmics/notes_search-algorithms.py
+++ algorithmics/notes_search-algorithms.py
@@ -3,4 +3,26 @@
 #Algorithms that take as input a list and a value for which to search, and produce as output the 
 #index or indices where that  value was found in the list.
 
-#Linear search, O(n)
+#Linear search, version 1, O(n) --------------------------------------------------------------------
+def linear(lst, text):
+    
+    for i in enumerate(lst):
+        if i[1] == text:
+            return i[0]
+    
+    return False
+
+a_list = [5, 1, 3, 6, 7, 3, 1, 6, 7, 8, 3, 6]
+print(linear(a_list, 6))
+
+#Linear search, version 2, O(n) --------------------------------------------------------------------
+def linear(a_list, an_item):
+    
+    for i in range(len(a_list)):
+        if a_list[i] == an_item:
+            return i       
+
+    return False
+
+a_list = [5, 1, 3, 6, 7, 3, 1, 6, 7, 8, 3, 6]
+print(linear(a_list, 6))
