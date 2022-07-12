b6c91950f6d895ca766fea91dd585b76c6bb2cc8
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-12 10:43:09 +0200

    Create >> notes_basics.py

---
+++ algorithmics/>> notes_basics.py
@@ -0,0 +1,47 @@
+
+#RECURSION
+
+#recursion_example_____________________________________________________________
+def count_down_1(start):
+     if start <= 0:
+         print(start)
+     else:
+         print(start)
+         count_down_1(start - 1)
+
+count_down_1(5)
+
+#recursion_example_____________________________________________________________
+def count_down_2(start):
+     if start <= 0:
+         print(start)
+     else:
+         count_down_2(start - 1)
+         print(start)
+         
+count_down_2(5)
+
+
+#SORTING ALGORITHMS
+
+#bubble_sort_algorithm_________________________________________________________
+
+def sort_with_bubbles(lst):
+    swap_occurred = True
+
+    while swap_occurred:
+
+        swap_occurred = False        
+
+        for i in range(len(lst) - 1):
+
+            if lst[i] > lst[i + 1]:                
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+
+                swap_occurred = True
+
+    return lst     
+
+print(sort_with_bubbles([4, 3, 5, 2, 1]))
