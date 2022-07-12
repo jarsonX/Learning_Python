d6daf262f39499b5ffb5c0f7036c7cac7413d886
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-12 14:50:55 +0200

    Create notes_search-algorithms.py

---
+++ algorithmics/notes_search-algorithms.py
@@ -0,0 +1,6 @@
+#-SEARCH-ALGORITHMS---------------------------------------------------------------------------------|
+
+#Algorithms that take as input a list and a value for which to search, and produce as output the 
+#index or indices where that  value was found in the list.
+
+#Linear search, O(n)
