25902e826eacbe47f94e3fe030b13054ed3ebede
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-18 12:12:04 +0200

    Update notes_search-algorithms.py

--- algorithmics/notes_search-algorithms.py
+++ algorithmics/notes_search-algorithms.py
@@ -145,45 +145,36 @@ def binary_search(searchList, searchTerm):
 #First, the list must be sorted.
     searchList.sort()
 
-#With each recursive call to binary_search, the size
-#of the list will be cut in half, rounding down. If
-#the search term is not found, then eventually an
-#empty list will be passed into binary_search. So,
-#if searchList is empty, we know that the search
-#term was not found, and we can return False. This
+#With each recursive call to binary_search, the size of the list will be cut in half, rounding down. 
+#If the search term is not found, then eventually an empty list will be passed into binary_search. So,
+#if searchList is empty, we know that the search #term was not found, and we can return False. This
 #is the base case for the recursive binary_search.
 
     if len(searchList) == 0:
         return False
 
-#If there are still items in the list, then we want
-#to find if searchTerm is greater than, less than,
-#or equal to the middle term in the list. For that,
-#we need the index of the middle term.
+#If there are still items in the list, then we want #to find if searchTerm is greater than, less than,
+#or equal to the middle term in the list. For that, #we need the index of the middle term.
 
     middle = len(searchList) // 2
 
-#First, the easy case: if it's the middle term, we
-#found it! Return True.
+#First, the easy case: if it's the middle term, we #found it! Return True.
     if searchList[middle] == searchTerm:
         return True
 
-#If the search term is less than the middle term,
-#then repeat the search on the left half of the
+#If the search term is less than the middle term, then repeat the search on the left half of the
 #list.
     elif searchTerm < searchList[middle]:
         return binary_search(searchList[:middle], searchTerm)
 
-#If the search term is greater than the middle
-#term, then repeat the search on the right half
+#If the search term is greater than the middle #term, then repeat the search on the right half
 #of the list.
     else:
         return binary_search(searchList[middle + 1:], searchTerm)
 
-#As long as there are items to be searched, binary_search
-#will keep getting called on smaller and smaller lists,
-#until eventually the item is found or the list of possible
-#places it could be found is empty.
+#As long as there are items to be searched, binary_search will keep getting called on smaller and 
+#smaller lists, until eventually the item is found or the list of possible places it could be 
+#found is empty.
 
 intlist = [12, 64, 23, 3, 57, 19, 1, 17, 51, 62]
 print("23 is in intlist:", binary_search(intlist, 23))
