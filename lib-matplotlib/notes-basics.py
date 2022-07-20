f0442ad28c46244b170bee78d62f9500ab655882
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-20 09:22:41 +0200

    Update notes-basics.py

--- lib-matplotlib/notes-basics.py
+++ lib-matplotlib/notes-basics.py
@@ -1,2 +1,36 @@
+#-MATPLOTLIB-FOR---------------------------------------------------------------------------------------#
+
+'''
+Matplotlib has 3 layers: scripting, artist and backend. For the most cases in data analysis, we're
+good with using the scripting layer (esentially the matplotlib.pyplot), which is intended to be 
+easy to use.
+
+'''
+
+
+#--PLOT-FUNCTION-----------------------------------------------------------------------------------#
+
+# Enables to create almost all of the conventional visualizations.
+
 import matplotlib.pyplot as plt
-%matplotlib inline  #Don't forget about "%matplotlib inline" to plot in a Jupyter notebook
+
+plt.plot(5, 5, 'o')
+plt.show()
+
+# NOTE: when using Jupyter notebook, we might need to force the graph to be printed within the 
+# browser by adding 'magic' function before importing matplotlib, i.e.:
+
+%matplotlib_inline
+import matplotlib.pyplot as plt
+
+# However, _inline backend keeps us from modifying graph once it is created and shown. To overcome
+# this, we can use %matplotlib_notebook backend instead. Then, when a function is called, it checks
+# if an active figure already exist and apply any effects to it.
+
+
+#--PANDAS------------------------------------------------------------------------------------------#
+
+# Pandas has a built-in implementation of matplotlib, therefore plotting is as simple as calling the
+# plot function on a given pandas series or dataframe.
+
+df_name.plot(kind='line')   or   df_name['column_1'].plot(kind='hist') etc.
